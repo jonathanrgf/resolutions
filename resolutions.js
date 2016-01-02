73a995da8a9393a53fb7bcdc8c6f25f0493f89a8
N3385 <joanthanrgf@gmail.com>
2016-01-01 19:23:20 -0500

    Sorting Data in a Collection

--- resolutions.js
+++ resolutions.js
@@ -1,17 +1,9 @@
+Resolutions = new Mongo.Collection('resolutions');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.helpers({
+    resolutions: function() {
+      return Resolutions.find();
     }
   });
 }
