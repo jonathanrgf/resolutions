9bdadf328753ec6057b82b5767acae0fc67dd3d5
N3385 <joanthanrgf@gmail.com>
2016-01-02 20:15:34 -0500

    added simple File Structure to project

--- resolutions.js
+++ resolutions.js
@@ -36,26 +36,7 @@ if (Meteor.isClient) {
     }
   });
 
-  // add helper 
-  Template.resolution.helpers({
-    isOwner: function() {
-      return this.owner === Meteor.userId();
-    }
-  });
-
-  Template.resolution.events({
-    // new event when checkbox is checked or unchecked
-    'click .toggle-checked': function() {
-      Meteor.call("updateResolution", this._id, !this.checked)
-    },
-    'click .delete': function() {
-      // call method to remove resolution passing id of resolution as parameter
-      Meteor.call("deleteResolution", this._id);
-    },
-    'click .toggle-private': function() {
-      Meteor.call("setPrivate", this._id, !this.private)
-    }
-  });
+ 
 
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
